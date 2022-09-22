d2f5f74781a7aced092b97ff3c431f207b658ff0
Matteo <mattez.salv@gmail.com>
2022-09-22 20:00:14 +0200

    aggiunto classe per la view delle system calls

---
+++ ThesisApp/SystemCallsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SystemCallsViewController.swift
+//  ThesisApp
+//
+//  Created by Matteo Salvatore on 22/09/22.
+//
+
+import UIKit
+
+class SystemCallsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
